4ac43c671adee67e87a0ce8eb26bc1d04d5c7895
Matt Bowman <mbowman@citrrus.com>
2013-12-04 10:57:19 -0500

    Left align the selected contacts

--- Sample/MBContactPicker/UICollectionViewContactFlowLayout.m
+++ Sample/MBContactPicker/UICollectionViewContactFlowLayout.m
@@ -8,6 +8,61 @@
 
 #import "UICollectionViewContactFlowLayout.h"
 
+const NSInteger kMaxCellSpacing = 9;
+
+// This is using the answer provided in the stack overflow post: http://bit.ly/INr0ie
+
 @implementation UICollectionViewContactFlowLayout
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSArray* attributesToReturn = [super layoutAttributesForElementsInRect:rect];
+    for (UICollectionViewLayoutAttributes* attributes in attributesToReturn) {
+        if (nil == attributes.representedElementKind) {
+            NSIndexPath* indexPath = attributes.indexPath;
+            attributes.frame = [self layoutAttributesForItemAtIndexPath:indexPath].frame;
+        }
+    }
+    return attributesToReturn;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewLayoutAttributes* currentItemAttributes =
+    [super layoutAttributesForItemAtIndexPath:indexPath];
+    
+    UIEdgeInsets sectionInset = [(UICollectionViewFlowLayout *)self.collectionView.collectionViewLayout sectionInset];
+    
+    if (indexPath.item == 0) { // first item of section
+        CGRect frame = currentItemAttributes.frame;
+        frame.origin.x = sectionInset.left; // first item of the section should always be left aligned
+        currentItemAttributes.frame = frame;
+        
+        return currentItemAttributes;
+    }
+    
+    NSIndexPath* previousIndexPath = [NSIndexPath indexPathForItem:indexPath.item-1 inSection:indexPath.section];
+    CGRect previousFrame = [self layoutAttributesForItemAtIndexPath:previousIndexPath].frame;
+    CGFloat previousFrameRightPoint = previousFrame.origin.x + previousFrame.size.width + kMaxCellSpacing;
+    
+    CGRect currentFrame = currentItemAttributes.frame;
+    CGRect strecthedCurrentFrame = CGRectMake(0,
+                                              currentFrame.origin.y,
+                                              self.collectionView.frame.size.width,
+                                              currentFrame.size.height);
+    
+    if (!CGRectIntersectsRect(previousFrame, strecthedCurrentFrame)) { // if current item is the first item on the line
+        // the approach here is to take the current frame, left align it to the edge of the view
+        // then stretch it the width of the collection view, if it intersects with the previous frame then that means it
+        // is on the same line, otherwise it is on it's own new line
+        CGRect frame = currentItemAttributes.frame;
+        frame.origin.x = sectionInset.left; // first item on the line should always be left aligned
+        currentItemAttributes.frame = frame;
+        return currentItemAttributes;
+    }
+    
+    CGRect frame = currentItemAttributes.frame;
+    frame.origin.x = previousFrameRightPoint;
+    currentItemAttributes.frame = frame;
+    return currentItemAttributes;
+}
 
 @end
