55d9c3af798ed2a7ef38318a587a83e693e4847a
Kirill Zdornyy <kirill@dineserve.com>
2015-10-21 14:52:06 -0700

    Fix conflict with UIView changes.
    New ivar “focused” conflicted with local definition.

--- MBContactPicker/MBContactCollectionView.m
+++ MBContactPicker/MBContactCollectionView.m
@@ -404,7 +404,7 @@ typedef NS_ENUM(NSInteger, ContactCollectionViewSection) {
 {
     MBContactCollectionViewContactCell *cell = (MBContactCollectionViewContactCell *)[collectionView cellForItemAtIndexPath:indexPath];
     [self becomeFirstResponder];
-    cell.focused = YES;
+    cell.mbfocused = YES;
     
     if ([self.contactDelegate respondsToSelector:@selector(contactCollectionView:didSelectContact:)])
     {
@@ -420,7 +420,7 @@ typedef NS_ENUM(NSInteger, ContactCollectionViewSection) {
 - (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     MBContactCollectionViewContactCell *cell = (MBContactCollectionViewContactCell *)[collectionView cellForItemAtIndexPath:indexPath];
-    cell.focused = NO;
+    cell.mbfocused = NO;
 }
 
 #pragma mark - UICollectionViewDelegateContactFlowLayout
@@ -502,11 +502,11 @@ typedef NS_ENUM(NSInteger, ContactCollectionViewSection) {
         cell.model = self.selectedContacts[[self selectedContactIndexFromIndexPath:indexPath]];
         if ([self.indexPathOfSelectedCell isEqual:indexPath])
         {
-            cell.focused = YES;
+            cell.mbfocused = YES;
         }
         else
         {
-            cell.focused = NO;
+            cell.mbfocused = NO;
         }
         collectionCell = cell;
     }
